9adc3e0be9ba1ad7731fff381d4ee3680b0d0836
luohy15 <luohycs@gmail.com>
2025-09-26 23:20:47 +0800

    fix typo

--- server/src/common.py
+++ server/src/common.py
@@ -58,7 +58,7 @@ def _make_api_request(function_name: str, params: dict) -> dict | str:
     api_params.update({
         "function": function_name,
         "apikey": get_api_key(),
-        "source": "alphavangtagemcp"
+        "source": "alphavantagemcp"
     })
     
     # Handle entitlement parameter if present in params or global variable
